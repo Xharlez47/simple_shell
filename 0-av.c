508cd74a2d9f40b94b010f7a3721514bcc3e5686
Xharlez47 <Charleskelvin47@gmail.com>
2022-11-16 10:24:05 -0800

    simple

--- 0-av.c
+++ 0-av.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - prints a arument
+ * @argc: numbers of arguments
+ * @argv: index position of the arguments
+ * Return: 0
+ */
+
 int main(int argc __attribute__((unused)), const char *argv[])
 {
 	unsigned int i = 0;
